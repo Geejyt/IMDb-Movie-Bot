9d53b183debfc1de7032da72b002ec94127a53a7
Aliou sow <89993689+Geejyt@users.noreply.github.com>
2022-03-07 18:22:37 +0000

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -21,7 +21,7 @@ async def inline_users(query: InlineQuery):
 
 @Client.on_inline_query()
 async def answer(bot, query):
-    """Show search results for given inline query"""
+    """Afficher les résultats de la recherche pour une requête en ligne donnée"""
     
     if not await inline_users(query):
         await query.answer(results=[],
@@ -33,8 +33,8 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
-                           switch_pm_parameter="subscribe")
+                           switch_pm_text='Vous devez rejoindre et acceptez les régles du groupe',
+                           switch_pm_parameter="Rejoindre")
         return
 
     results = []
@@ -89,7 +89,7 @@ async def answer(bot, query):
         except Exception as e:
             logging.exception(str(e))
     else:
-        switch_pm_text = f'{emoji.CROSS_MARK} No results'
+        switch_pm_text = f'{emoji.CROSS_MARK} Pas de résultats'
         if string:
             switch_pm_text += f' for "{string}"'
 
@@ -97,13 +97,13 @@ async def answer(bot, query):
                            is_personal = True,
                            cache_time=cache_time,
                            switch_pm_text=switch_pm_text,
-                           switch_pm_parameter="okay")
+                           switch_pm_parameter="ok")
 
 
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('Recherchez', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
