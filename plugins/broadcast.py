5f0bab73bf655d18bda437946adae29253c60e61
Aliou sow <89993689+Geejyt@users.noreply.github.com>
2022-03-07 20:31:27 +0000

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -12,7 +12,7 @@ async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
-        text='Broadcasting your messages...'
+        text='Diffuser vos messages...'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
@@ -27,15 +27,15 @@ async def verupikkals(bot, message):
         if pti:
             success += 1
         elif pti == False:
-            if sh == "Blocked":
+            if sh == "Blockée":
                 blocked+=1
-            elif sh == "Deleted":
+            elif sh == "Supprimée":
                 deleted += 1
-            elif sh == "Error":
+            elif sh == "Erreur":
                 failed += 1
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+            await sts.edit(f"Diffusion en cours:\n\nUtilisateurs totales {total_users}\nComplet: {done} / {total_users}\nSuccés: {success}\nBlockées: {blocked}\nSupprimée: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    await sts.edit(f"Diffusion Completet:\nComplet en {time_taken} secondes.\n\nUtilisateurs totales {total_users}\nComplet: {done} / {total_users}\nSuccés: {success}\nBlockées: {blocked}\nSupprimée: {deleted}")
